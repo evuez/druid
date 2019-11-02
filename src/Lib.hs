12ae51af9827e60920582a096091bc5c3bdda420
evuez <helloevuez@gmail.com>
2019-11-02 10:25:22 +0100

    Remove unused imports

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,7 @@
 module Lib where
 
-import Control.Applicative ((<|>), empty, liftA2, liftA3, optional)
-import Control.Monad (MonadPlus, (<$!>), void)
-import Control.Monad.Combinators.Expr (makeExprParser)
-import Control.Monad.Combinators.Expr (Operator(InfixL, InfixR, Prefix))
-import Data.Char (isSpace)
+import Control.Applicative ((<|>), empty, liftA2)
+import Control.Monad ((<$!>))
 import Data.Void
 import qualified Expr as E (EExpr(..), Operator(..))
 import Text.Megaparsec
@@ -13,35 +10,25 @@ import Text.Megaparsec
   , between
   , count'
   , eof
-  , lookAhead
   , many
   , manyTill
-  , notFollowedBy
   , oneOf
   , parse
   , sepBy
-  , sepBy1
-  , sepEndBy
-  , sepEndBy1
-  , takeWhile1P
   , try
   )
 import qualified Text.Megaparsec.Char as C
   ( char
   , digitChar
-  , eol
   , letterChar
-  , lowerChar
   , space1
   , string
-  , upperChar
   )
 import qualified Text.Megaparsec.Char.Lexer as L
   ( charLiteral
   , decimal
   , float
   , lexeme
-  , skipLineComment
   , space
   , symbol
   )
@@ -49,50 +36,29 @@ import Text.Megaparsec.Error (errorBundlePretty)
 
 type Parser = Parsec Void String
 
-sepEndBy2 :: MonadPlus f => f a -> f sep -> f [a]
-sepEndBy2 p sep = liftA2 (:) (p <* sep) (sepEndBy1 p sep)
-
 --
 -- Lexer
 --
 spaceConsumer :: Parser ()
-spaceConsumer = L.space space lineComment blockComment
-  where
-    lineComment = L.skipLineComment "#"
-    blockComment = empty
-
-spaceConsumer' :: Parser ()
-spaceConsumer' = L.space C.space1 lineComment blockComment
-  where
-    lineComment = L.skipLineComment "#"
-    blockComment = empty
-
-space :: Parser ()
-space = void $ oneOf " \t"
+spaceConsumer = L.space C.space1 empty empty
 
 lexeme :: Parser a -> Parser a
 lexeme = L.lexeme spaceConsumer
 
-lexeme' :: Parser a -> Parser a
-lexeme' = L.lexeme spaceConsumer'
-
 symbol :: String -> Parser String
 symbol = L.symbol spaceConsumer
 
-symbol' :: String -> Parser String
-symbol' = L.symbol spaceConsumer'
-
 braces :: Parser a -> Parser a
-braces = between (symbol' "{") (symbol' "}")
+braces = between (symbol "{") (symbol "}")
 
 squareBrackets :: Parser a -> Parser a
-squareBrackets = between (symbol' "[") (lexeme (optional C.eol) *> symbol' "]")
+squareBrackets = between (symbol "[") (symbol "]")
 
 integer :: Parser Integer
-integer = lexeme L.decimal -- TODO: should allow `_`
+integer = lexeme L.decimal
 
 float :: Parser Float
-float = lexeme L.float -- TODO: should allow `_`
+float = lexeme L.float
 
 string :: Parser String
 string = lexeme strictString
@@ -165,7 +131,8 @@ unquotedAtom =
    (C.string "!=") <|>
    (C.string "!") <|>
    unquotedAtomBody) <|>
-  (C.string "Access")
+  (C.string "Access") <|>
+  (C.string "Elixir")
 
 quotedAtomBody :: Parser String
 quotedAtomBody = charlist <|> string
@@ -178,18 +145,17 @@ specialAtom = lexeme $ symbol "true" <|> symbol "false" <|> symbol "nil"
 
 --
 keywords :: (E.EExpr -> E.EExpr -> b) -> Parser b
-keywords wrapper = do
-  key <- E.Atom <$!> (quotedAtomBody <|> unquotedAtomBody)
-  void $ symbol' ":"
-  value <- parseAny
-  void $ optional (lexeme C.eol)
-  pure $ wrapper key value
+keywords wrapper =
+  liftA2
+    wrapper
+    (E.Atom <$!> (quotedAtomBody <|> unquotedAtomBody))
+    (symbol ": " *> parseAny)
 
 listKeywords :: Parser E.EExpr
 listKeywords = keywords (\k v -> E.Tuple [k, v])
 
 commaSeparated :: Parser a -> Parser [a]
-commaSeparated parser = parser `sepBy` (symbol' ",")
+commaSeparated parser = parser `sepBy` (symbol ",")
 
 --
 -- Parser
