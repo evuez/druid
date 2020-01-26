8157da4d0e910e4c5e607b91938d0df18c7f3212
evuez <helloevuez@gmail.com>
2020-01-26 19:24:37 +0100

    Add def* in B.reify

--- src/Expr/Base.hs
+++ src/Expr/Base.hs
@@ -61,10 +61,20 @@ doReify (Integer a) = C.Integer a
 doReify (List a) = C.List $ reify <$> a
 doReify (Map a) = C.Map $ both reify <$> a
 doReify (MapUpdate a b) = C.MapUpdate (reify a) (both reify <$> b)
-doReify (NonQualifiedCall "defmodule" (x:xs)) =
-  case runWriter x of
-    ((Alias _), _) -> C.Module (reify x) (C.BlockVal $ reify <$> xs)
-    _ -> error "Invalid module definition." -- NonQualifiedCall "defmodule" ... + errors ["Invalid module def"]?
+doReify (NonQualifiedCall a b@(x:xs)) =
+  case (a, runWriter x) of
+    ("defmodule", ((Alias _), _)) -> C.Module (reify x) (C.BlockVal $ reify <$> xs)
+    ("defmodule", _) -> error "Invalid module definition." -- NonQualifiedCall "defmodule" ... + errors ["Invalid module def"]?
+    ("def", ((Variable name'), _)) -> C.Def name' [] (C.BlockVal $ reify <$> xs)
+    ("def", ((NonQualifiedCall name' args'), _)) -> C.Def name' (reify <$> args') (C.BlockVal $ reify <$> xs)
+    ("def", _) -> error "Invalid function definition."
+    ("defp", ((Variable name'), _)) -> C.DefP name' [] (C.BlockVal $ reify <$> xs)
+    ("defp", ((NonQualifiedCall name' args'), _)) -> C.DefP name' (reify <$> args') (C.BlockVal $ reify <$> xs)
+    ("defp", _) -> error "Invalid private function definition."
+    ("defmacro", ((Variable name'), _)) -> C.DefMacro name' [] (C.BlockVal $ reify <$> xs)
+    ("defmacro", ((NonQualifiedCall name' args'), _)) -> C.DefMacro name' (reify <$> args') (C.BlockVal $ reify <$> xs)
+    ("defmacro", _) -> error "Invalid macro definition."
+    _ -> C.NonQualifiedCall a (reify <$> b)
 doReify (NonQualifiedCall a b) = C.NonQualifiedCall a (reify <$> b)
 doReify (QualifiedCall a b c) = C.QualifiedCall (reify a) b (reify <$> c)
 doReify (AnonymousCall a b) = C.AnonymousCall (reify a) (reify <$> b)
